c061d4746a4b0d5ac51b69ac95a2735105bc9f92
Adalto Selau Sparremberger <adaltoss@me.com>
2021-08-07 10:56:26 -0300

    Aula02 - 07/08/2021 - Manhã

---
+++ Aula02/Aula02/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Aula02
+//
+//  Created by Adalto Selau Sparremberger on 07/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func alterarCor() {
+        
+    }
+    
+
+}
+
